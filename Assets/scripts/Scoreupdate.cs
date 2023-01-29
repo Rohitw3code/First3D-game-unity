892cf8eac72fd8f2abca8adcd83dd51ef772c55f
rohitw3code <rohitcode005@gmail.com>
2023-01-29 13:52:00 +0530

    added score counter

---
+++ Assets/scripts/Scoreupdate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class Scoreupdate : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI score_;
+
+    void Update() {
+
+        score_.text = player.position.z.ToString("0");
+        
+    }
+}
